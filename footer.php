d6b65d8b1a7f62e888a66132b0132853e72f3611
huangqi-hy <1095332843@qq.com>
2014-10-20 12:58:33 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div id="footer">
+    <hr />
+    
+    <div id='xiaoxun'><img src='images/xiaoxun.png'></div>
+    
+	  <p>更多植物知识：<a href="http://frps.eflora.cn/" target="_blank">中国在线植物志</a></p>
+	  
+</div>
+
+</body>
+</html>
