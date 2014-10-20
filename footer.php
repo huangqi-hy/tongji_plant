fa7c18efc4f546a9953856286639c432ee60afff
huangqi-hy <1095332843@qq.com>
2014-10-20 13:19:48 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,9 @@
-<div id="footer">
-    <hr />
+<footer>
     
-    <div id='xiaoxun'><img src='images/xiaoxun.png'></div>
     
 	  <p>更多植物知识：<a href="http://frps.eflora.cn/" target="_blank">中国在线植物志</a></p>
 	  
-</div>
+</footer>
 
 </body>
 </html>
